ee490dce2709c54c32a2f626779891c95f2cce04
Garrett Hill <ghill012@gmail.com>
2020-07-05 16:44:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,7 +8,10 @@ def loop_message_five_times(string)
 end
 
 def loop_message_n_times(string, integer)
-
+  count = 0 
+  
+  while count < integer do
+    puts string
+    count += 1 
+  end
 end
-  
-  
